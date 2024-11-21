c6abe9b36bf42e1e97fb1aaa903ee1251d6fe661
Moussa Bamba <moussabulls1@gmail.com>
2024-11-21 15:55:58 +0100

    refactor(friendship.view => class based)

--- friendships/urls.py
+++ friendships/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from . import views
+from .views import FriendshipView
 
 urlpatterns = [
-    path('friends/', views.user_list_view, name='friends'),
-    path("add_friend/<int:user_id>/", views.add_friend, name="add_friend"),
-    path("remove_friend/<int:user_id>/", views.remove_friend, name="remove_friend"),
-    path("accept_friend_request/<int:friendship_id>/", views.accept_friend_request, name="accept_friend_request"),
-    path("reject_friend_request/<int:friendship_id>/", views.reject_friend_request, name="reject_friend_request"),
+    path('friends/', FriendshipView.user_list_view, name='friends'),
+    path("add_friend/<int:user_id>/", FriendshipView.add_friend, name="add_friend"),
+    path("remove_friend/<int:user_id>/", FriendshipView.remove_friend, name="remove_friend"),
+    path("accept_friend_request/<int:friendship_id>/", FriendshipView.accept_friend_request, name="accept_friend_request"),
+    path("reject_friend_request/<int:friendship_id>/", FriendshipView.reject_friend_request, name="reject_friend_request"),
 ]
--- friendships/views.py
+++ friendships/views.py
@@ -2,15 +2,14 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 from .models import User, Friendship
+from django.views import View
 
-@login_required
-def user_list_view(request):
+class FriendshipView:
+ @login_required
+ def user_list_view(request):
     current_user = request.user
-
-    # Récupérer tous les utilisateurs sauf l'utilisateur connecté
     users = User.objects.exclude(id=current_user.id)
 
-    # Déterminer le statut d'amitié pour chaque utilisateur
     user_statuses = []
     for user in users:
         friendship = Friendship.objects.filter(
@@ -32,7 +31,6 @@ def user_list_view(request):
             "status": status
           })
 
-    # Récupérer les demandes d'amitié en attente
     pending_friend_requests = Friendship.objects.filter(recever_user=current_user, approved=False)
 
     context = {
@@ -44,14 +42,14 @@ def user_list_view(request):
     return render(request, "friends.html", context)
 
 
-@login_required
-def add_friend(request, user_id):
+ @login_required
+ def add_friend(request, user_id):
     receiver = get_object_or_404(User, id=user_id)
     Friendship.objects.create(sender_user=request.user, recever_user=receiver)
     return redirect("friends")
 
-@login_required
-def remove_friend(request, user_id):
+ @login_required
+ def remove_friend(request, user_id):
     other_user = get_object_or_404(User, id=user_id)
     Friendship.objects.filter(
         Q(sender_user=request.user, recever_user=other_user) | 
@@ -59,17 +57,15 @@ def remove_friend(request, user_id):
     ).delete()
     return redirect("friends")
 
-@login_required
-def accept_friend_request(request, friendship_id):
-    """Accepter une demande d'amitié."""
+ @login_required
+ def accept_friend_request(request, friendship_id):
     friendship = get_object_or_404(Friendship, id=friendship_id, recever_user=request.user)
     friendship.approved = True
     friendship.save()
     return redirect("friends")
 
-@login_required
-def reject_friend_request(request, friendship_id):
-    """Refuser une demande d'amitié (supprimer la demande)."""
+ @login_required
+ def reject_friend_request(request, friendship_id):
     friendship = get_object_or_404(Friendship, id=friendship_id, recever_user=request.user)
     friendship.delete()
     return redirect("friends")
